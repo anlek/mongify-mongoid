90d2608cb963a7429855dc26f5f9e046caa0f9f2
Andrew Kalek <andrew.kalek@anlek.com>
2013-04-02 20:03:41 -0400

    Added base exceptions

---
+++ lib/mongify/mongoid/exceptions.rb
@@ -0,0 +1,21 @@
+module Mongify
+    module Mongoid
+    # Base Mongify Error
+    class Error < RuntimeError; end
+    
+    # Not Implemented Error from Mongify
+    class NotImplementedError < Error; end
+    
+    # File Not Found Exception
+    class FileNotFound < Error; end
+    # Raised when Translation file is missing
+    class TranslationFileNotFound < FileNotFound; end
+    
+    
+    # Raised when application has no root folder set
+    class RootMissing < Error; end
+    
+    # Raised when an invalid option is passed via CLI
+    class InvalidOption < Error; end
+  end
+end
