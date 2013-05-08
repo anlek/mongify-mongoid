9263624bfb02df932dadddf15936dc75a17dc426
Andrew Kalek <andrew.kalek@anlek.com>
2013-05-07 21:31:09 -0400

    Testing process works

--- spec/lib/mongify/mongoid/generator_spec.rb
+++ spec/lib/mongify/mongoid/generator_spec.rb
@@ -4,6 +4,18 @@ describe Mongify::Mongoid::Generator do
   context "Instance Methods" do
     context "process" do
 
+      let(:translation_file) { "spec/files/translation.rb" }
+      subject(:generator) { Mongify::Mongoid::Generator.new(translation_file) }
+
+      it "processes successfully" do
+        subject.should_receive(:generate_root_models)
+        subject.should_receive(:generate_embedded_models)
+        subject.should_receive(:generate_polymorphic_models)
+        subject.should_receive(:write_models_to_file)
+
+        subject.process
+      end
+
       context "file doesnt exists" do
         subject { Mongify::Mongoid::Generator.new("/my/awesome/file.ext") }
 
