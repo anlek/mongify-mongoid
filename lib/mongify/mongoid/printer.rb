df0d4af753e2e9c077f33e2cb10b95acb046a764
Andrew Kalek <andrew.kalek@anlek.com>
2013-05-28 13:40:22 -0400

    Added docs

--- lib/mongify/mongoid/printer.rb
+++ lib/mongify/mongoid/printer.rb
@@ -2,7 +2,7 @@ require 'erb'
 
 module Mongify
   module Mongoid
-    # Class that writes the models to files
+    # Class that writes the models to individule mongoid model files
     class Printer
       TEMPLATE_FILE = File.expand_path('templates/mongoid.rb.erb', File.dirname(__FILE__))
       attr_accessor :models, :output_directory
@@ -11,6 +11,7 @@ module Mongify
         @output_directory = output_directory
       end
 
+      # Writes models to given output directory
       def write
         models.each do |key, model|
           write_file(model)
@@ -21,19 +22,23 @@ module Mongify
       private
       #######
       
+      # Returns ERB template file
       def template
         @template ||= File.read(TEMPLATE_FILE)
       end
       
+      # Writes given model to output file
       def write_file model
         output = render_file model
         save_file output, model.name
       end
 
+      # Renders ERB template for given model
       def render_file model
         ERB.new(template, nil, '-').result(model.get_binding)
       end
 
+      # Saveds text output into a file (output_directory/model_name.rb)
       def save_file output, file_name
         File.open(File.join(output_directory, "#{file_name.downcase}.rb"), 'w') {|f| f.write(output) }
       end
