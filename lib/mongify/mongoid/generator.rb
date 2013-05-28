ce58bb63945bac1ab60ba376bf1161bbde38c04d
Andrew Kalek <andrew.kalek@anlek.com>
2013-05-28 13:30:53 -0400

    Add docs small code cleanup

--- lib/mongify/mongoid/generator.rb
+++ lib/mongify/mongoid/generator.rb
@@ -8,6 +8,7 @@ module Mongify
         @models = {}
       end
 
+      #Process translation file and generate output files
       def process
         unless File.exists?(@translation_file)
           raise Mongify::Mongoid::TranslationFileNotFound, "Unable to find Translation File at #{@translation_file}"
@@ -19,12 +20,14 @@ module Mongify
         write_models_to_file
       end
 
+      #Generate models based on traslation tables
       def generate_models
         (translation.tables + translation.polymorphic_tables).each do |table|
           build_model(table)
         end
       end
 
+      #Goes through all the models and adds fields based on columns in the given table
       def process_fields
         models.each do |key, model|
           table = translation.find(model.table_name)
@@ -32,20 +35,24 @@ module Mongify
         end
       end
 
+      #Goes through embedded relationships
       def generate_embedded_relations
         translation.embed_tables.each do |table|
           extract_embedded_relations(table)
         end
       end
 
+      # Writes models to files
       def write_models_to_file
         Printer.new(models, @output_dir).write
       end
 
+      #Returns model based on table name
       def find_model(name)
         @models[name.to_s.downcase.to_sym]
       end
 
+      #Returns Mongify translation class for given translation file
       def translation
         @translation ||= Mongify::Translation.parse(@translation_file)
       end
@@ -54,8 +61,7 @@ module Mongify
       private
       #######
       
-      
-
+      # generates fields for given model and it's table
       def generate_fields_for(model, table)
         table.columns.each do |column|
           if column.options['references'] && parent_model = find_model(column.options['references'])
@@ -68,6 +74,7 @@ module Mongify
         end
       end
 
+      #Extracts embedded relationships for given table
       def extract_embedded_relations(table)
         model = find_model(table.name)
         parent_model = find_model(table.embed_in)
@@ -77,7 +84,7 @@ module Mongify
         model
       end
 
-
+      #Returns build model based on a table
       def build_model(table)
         model = Mongify::Mongoid::Model.new(class_name: table.name.classify, table_name: table.name)
         model.polymorphic_as = table.polymorphic_as if table.polymorphic?
@@ -85,8 +92,6 @@ module Mongify
         @models[table.name.downcase.to_sym] = model
         model
       end
-
-
     end
   end
 end
